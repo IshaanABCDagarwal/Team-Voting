7ffc426cdc7f4ac0a87ebb61e614235dd4e9755d
IshaanABCDagarwal <iragarwal3@gmail.com>
2021-12-28 19:34:46 +0530

    This is a sample message

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  
+  apiKey: "AIzaSyDrqqq9qZZkwZHGWbeTjFZKZ5YTvpyMmt4",
+  authDomain: "pro-c67-e310f.firebaseapp.com",
+  projectId: "pro-c67-e310f",
+  storageBucket: "pro-c67-e310f.appspot.com",
+  messagingSenderId: "765201852883",
+  appId: "1:765201852883:web:98c289cfa86a3f5b88e726"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
